3558fff923f0b1442c9da5d989585ed804276ae9
4len <144851980+femboyPig@users.noreply.github.com>
2024-08-11 09:21:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
     var db = document.getElementById('downloadButton');
     var hdl = document.getElementById('hiddenDownloadLink');
-    var url = 'https://cdn.modrinth.com/data/ntmv3V8v/versions/VMRuYl1H/brush-client-1.4.6.jar';
-    db.addEventListener('click', function(e) {
+
+    async function releaseUrl() {
+        try {
+            const response = await fetch('https://api.github.com/repos/femboypig/brush-client/releases/latest');
+            if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+            const data = await response.json();
+            return data.assets.find(asset => asset.name.includes("jar")).browser_download_url;
+        } catch (error) {
+            console.error("Failed to fetch the latest release URL:", error);
+            return null;
+        }
+    }
+
+    db.addEventListener('click', async function(e) {
         e.preventDefault();
-        hdl.href = url;
-        hdl.click();
+        const url = await releaseUrl();
+        if (url) {
+            hdl.href = url;
+            hdl.click();
+        } else {
+            alert('Failed to find the download URL.');
+        }
     });
 });
